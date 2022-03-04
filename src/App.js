00a60cd1546f78eaf1da30fe001e49489911d27e
AndriyCh1 <chekanovskiyacc@gmail.com>
2022-03-04 12:45:58 +0200

    feat: web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Redirect,
+  Switch 
+} from "react-router-dom";
 
-function App() {
+import Tours from './pages/Tours';
+import Navigation from './parts/Navigation/Navigation';
+import Guides from './pages/Guides';
+import Categories from './pages/Categories';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navigation/>
+      <main>
+        <Switch>
+        <Route path="/" exact>
+          <Tours/>
+        </Route>
+        <Route path="/guides" exact>
+          <Guides/>
+        </Route>
+        <Route path="/categories" exact>
+          <Categories/>
+        </Route>
+        <Redirect to="/"/>
+        </Switch>
+      </main>
+    </Router>
+    );
 }
 
 export default App;
---
+++ src/components/GuideList/GuideList.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Button from "../../parts/UIElements/Button";
+import Guide from "../Guide/Guide";
+import GuideModalForm from "../ModalForm/GuideModalForm";
+import "./GuideList.css"
+const baseURL = "https://agency-api31.herokuapp.com";
+
+const GuideList = (props) =>  {
+    const [guideModal, setGuideModal] = useState(false);
+    const [editId, setEditId] = useState(0);
+    const [filterValue, setFilterValue] = useState("");
+
+    const deleteGuide = (id) => {
+      axios.delete(`${baseURL}/guides/${id}`).then((response) => {
+        props.mutateGuides();
+      })
+    }
+
+    const ActionButtons = (id) => {
+      return(
+        <div className="guide-action">
+          <Button onClick={() => {setGuideModal(true); setEditId(id);}}>Edit</Button>
+          <Button onClick={() => {deleteGuide(id)}}>Delete</Button>
+        </div>
+      );
+    }
+  
+  
+    return (
+    <>
+      {guideModal? <GuideModalForm 
+        guideModal={guideModal}
+        setGuideModal={setGuideModal}
+        editId={editId}
+        mutateGuides={props.mutateGuides}
+    />: null}
+    
+    <div className ="guides">
+     <div className="filter-form">
+        <div>
+          <Button 
+          onClick={() => {
+            setEditId(0); 
+            setGuideModal(true);
+
+          }}
+          style={{backgroundColor: "#9ff2fc", marginRight:"10px"}}>
+            Додати гіда
+          </Button>
+        </div>
+
+        <div className="filter-action">
+          <input
+            type="text" 
+            placeholder="Пошук..." 
+            className="search-input" 
+            onChange={(e) => {
+              setFilterValue(e.target.value)
+            }}
+          />
+          </div>
+      </div>
+
+      <ul className="guide-list">
+        {props.guides?.filter((guide) => 
+            guide?.first_name === "" || 
+            guide.first_name?.toLowerCase().includes(filterValue.toLowerCase()) ||
+            guide?.last_name === "" || 
+            guide.last_name?.toLowerCase().includes(filterValue.toLowerCase()))
+          .map(el => 
+        <li key={el.guide_id} className="guide-item">
+            <Guide 
+              key={el.guide_id}
+              firstName= {el?.first_name}
+              lastName= {el?.last_name}
+              age= {el.age}
+              experience={el.experience}
+              about={el.about}
+            />
+          {ActionButtons(el.guide_id)}
+        </li>
+        )}
+      </ul>
+      </div>
+      </>
+    )
+}
+
+export default GuideList;
